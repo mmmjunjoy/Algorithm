537c42cbcece06afb7ed2fef442e5f30bf0577dc
mmmjunjoy <sjbssk0809@gmail.com>
2023-11-08 11:32:22 +0900

    Beakjoon.2755.refacoring

--- BAEKJOON/2755.평점.py
+++ BAEKJOON/2755.평점.py
@@ -16,18 +16,13 @@
 
 
 
-# 입력값
 
-# 7
-# General_Physics_1 3 A+
-# Introduction_to_Computer_Science_and_Eng 3 B0
-# Reading_And_Writing 2 C0
-# English_1 3 C+
-# Analytic_Geometry_and_Calculus_1 3 B+
-# Fortran_Programming 3 B+
-# C_Language_Programming 3 A+
 
 #-------------------------start-----------------------
+
+#0.5반올림해주기 위해서
+# import decimal
+
 
 # 필요한 변수 지정
 
@@ -40,6 +35,9 @@ point = []
 # 최종합
 bigpoint = 0  
 
+# 최종듣는수업시간합
+pluspoint = 0
+
 # 첫 입력값
 a = int(input())
 
@@ -47,44 +45,70 @@ a = int(input())
 for i in range(a):
   b = input()
   lens = len(b)
-  gradeplus.append(b[lens-2:lens-1])
+  gradeplus.append(b[lens-2])
+  gradeplus.append(b[lens-1])
   print('과목당 점수' , gradeplus)
   point.append(b[lens-4])
   print("과목당 수업시수", point)
 
-  if gradeplus[0] == "A+":
-    # 실수형 변환학점
-    pointf = 4.3
-  elif gradeplus[0] =="A0":
-    pointf = 4.0
-  elif gradeplus[0] =="A-":
-    pointf = 3.7
-  elif gradeplus[0] =="B+":
-    pointf = 3.3
-  elif gradeplus[0] =="B0":
-    pointf = 3.0
-  elif gradeplus[0] =="B-":
-    pointf = 2.7
-  elif gradeplus[0] =="C+":
-    pointf = 2.3
-  elif gradeplus[0] =="C0":
-    pointf = 2.0
-  elif gradeplus[0] =="C-":
-    pointf = 1.7
-  elif gradeplus[0] =="D+":
-    pointf = 1.3
-  elif gradeplus[0] =="D0":
-    pointf = 1.0
-  elif gradeplus[0] =="D-":
-    pointf = 0.7
+ # 실수형 변환학점
+  if gradeplus[0] == "A":
+    if gradeplus[1] == "+":
+      pointf = 4.3
+    elif gradeplus[1] == "-":
+      pointf = 3.7
+    else :
+      pointf = 4.0
+  elif gradeplus[0] == "B":
+    if gradeplus[1] == "+":
+      pointf = 3.3
+    elif gradeplus[1] == "-":
+      pointf = 2.7
+    else :
+      pointf = 3.0
+  elif gradeplus[0] == "C":
+    if gradeplus[1] == "+":
+      pointf = 2.3
+    elif gradeplus[1] == "-":
+      pointf = 1.7
+    else :
+      pointf = 2.0
+  elif gradeplus[0] == "D":
+    if gradeplus[1] == "+":
+      pointf = 1.3
+    elif gradeplus[1] == "-":
+      pointf = 0.7
+    else :
+      pointf = 1.0
   else:
     pointf = 0.0
 
-  xy = int(point[0])*pointf
+  xy = float(point[0])*float(pointf)
+  # print("한과목당 합", xy)
+  bigpoint += xy
+  # print("나누기전 점수", bigpoint)
+  pluspoint += float(point[0])
+  # print("최종 듣는 수업 시간 합", pluspoint)
   gradeplus =[]
   point = []
-bigpoint += xy
 
-lastpoint = bigpoint/a
+lastpoint = bigpoint/pluspoint
 
-print(lastpoint)
+
+# round 사용하여 , 소수점 둘쨰자리까지 출력
+
+print("반올림 전" , lastpoint)
+print("가공" , round(lastpoint + 10**-10,2))
+print(round(lastpoint + 10**-10,2))
+
+
+# 입력값
+
+# 7
+# General_Physics_1 3 A+
+# Introduction_to_Computer_Science_and_Eng 3 B0
+# Reading_And_Writing 2 C0
+# English_1 3 C+
+# Analytic_Geometry_and_Calculus_1 3 B+
+# Fortran_Programming 3 B+
+# C_Language_Programming 3 A+
---
+++ BAEKJOON/2755.평점.refac.py
@@ -0,0 +1,16 @@
+sub = int(input())
+total_num = sc = 0
+grade_arr = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D+', 'D0', 'D-', 'F']
+score_arr = [4.3, 4, 3.7, 3.3, 3, 2.7, 2.3, 2, 1.7, 1.3, 1, 0.7, 0]
+
+
+for i in range(sub):
+    s, num, gr = input().split(" ")
+
+    total_num += int(num)
+    print("과목성적- 영어" ,gr)
+    print("과목성적 - 인덱스 " ,grade_arr.index(gr))
+    print("과목성적 - 숫자" ,score_arr[grade_arr.index(gr)])
+    sc += (score_arr[grade_arr.index(gr)] * int(num))
+
+print("%.2f" % round(sc/total_num + 10**-10, 2))
