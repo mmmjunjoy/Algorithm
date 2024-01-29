325545b50b0f1564341f3c9b475538de61b50382
JunBo Sim <121990539+mmmjunjoy@users.noreply.github.com>
2024-01-29 20:08:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.70 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    
+    hashDict ={}
+    sumHash = 0
+    
+
+    for part in participant:
+        hashDict[hash(part)] = part
+        sumHash += hash(part)
+    
+    for comp in completion:
+        sumHash -= hash(comp)
+        
+    
+    print(sumHash)
+            
+    answer = ''
+    return hashDict[sumHash]
