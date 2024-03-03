a4334d3a83e062e42d41542d7d5d98a0f19dbc11
JunBo Sim <121990539+mmmjunjoy@users.noreply.github.com>
2024-03-03 17:20:26 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2100 ms, Memory: 42388 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,44 @@
+
+
+import sys
+
+
+input = sys.stdin.readline
+
+
+
+n = int(input())
+
+
+namelist = set()
+for i in range(n):
+    name,state = map(str, input().split())
+
+    if name in namelist:
+        if state =="leave":
+            namelist.remove(name)
+
+    if name not in namelist :
+        if state == "enter":
+            namelist.add(name)
+
+# print(namelist)
+namelist = list(namelist)
+
+# print(namelist)
+    
+namelist.sort(reverse=True)
+
+# print(namelist)
+
+# for i in namelist:
+#     print(i)
+
+
+s =""
+
+for i in namelist:
+    s += str(i) + '\n'
+
+print(s)
+    
