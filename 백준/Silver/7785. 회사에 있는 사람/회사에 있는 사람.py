b3b3a485b3371dfa148ac532a41e5d576f428a54
JunBo Sim <121990539+mmmjunjoy@users.noreply.github.com>
2024-03-03 17:24:50 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2052 ms, Memory: 42388 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -10,6 +10,11 @@ input = sys.stdin.readline
 n = int(input())
 
 
+
+# remove를 사용해야 하므로, list가 아닌 dict형태의 set을 사용하자
+
+# list의 시간복잡도 / remove -o(n) 
+# set의 시간복잡도 / remove - o(1) 이다.
 namelist = set()
 for i in range(n):
     name,state = map(str, input().split())
@@ -22,19 +27,24 @@ for i in range(n):
         if state == "enter":
             namelist.add(name)
 
-# print(namelist)
+
+# 정렬 사용하기 위해, list로 변환해준다. -set을
 namelist = list(namelist)
 
-# print(namelist)
-    
+
+
+
+# 역순정렬을 위해 sort ,reverse사용해주고,
+
 namelist.sort(reverse=True)
 
-# print(namelist)
+
 
 # for i in namelist:
 #     print(i)
 
 
+# 시간복잡도 위해 위에것을 아래로 변경
 s =""
 
 for i in namelist:
